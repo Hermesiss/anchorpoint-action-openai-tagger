fde05322ed1b948771e0be7562b6031f0ea2cac3
Hermesis <hermesiss@gmail.com>
2024-12-21 21:48:29 +0100

    Tag files recursively

--- tag_file_ai.py
+++ tag_file_ai.py
@@ -1,6 +1,6 @@
 import base64
 import json
-from typing import Any
+from typing import Any, Union
 
 import anchorpoint as ap
 import apsync as aps
@@ -8,6 +8,7 @@ import os
 import tempfile
 import random
 import openai
+import hashlib
 
 import tiktoken
 
@@ -33,6 +34,7 @@ input_token_price = 0.00000015
 # $0.00765 for 1 million pixels
 input_pixel_price = 0.00765 / 1000000
 output_token_price = 0.00000016
+images_per_request = 10
 proceed_dialog: ap.Dialog
 
 engines_variants = [
@@ -120,6 +122,16 @@ response_format = {"type": "json_schema", "json_schema":
     }}
 
 
+def calculate_file_hash(file_path, hash_algorithm="sha256"):
+    hash_func = hashlib.new(hash_algorithm)
+
+    with open(file_path, "rb") as f:
+        while chunk := f.read(8192):
+            hash_func.update(chunk)
+
+    return hash_func.hexdigest()
+
+
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
@@ -127,27 +139,31 @@ def encode_image(image_path):
 
 def create_temp_directory():
     # Create a temporary directory
-    temp_dir = tempfile.mkdtemp(None, "anchorpoint_")
+    temp_dir_root = os.path.join(tempfile.gettempdir(), "anchorpoint", "ai_tagger", "previews")
+    print(f"Temp dir root: {temp_dir_root}")
+    if not os.path.exists(temp_dir_root):
+        os.makedirs(temp_dir_root)
 
-    return temp_dir
+    return temp_dir_root
 
 
-def get_thumbnail_image(workspace_id, input_path, output_folder):
-    # create temporary folder
-
-    # generate the thumbnail which is a png file and put it in the temporary directory
-    aps.generate_thumbnails(
-        [input_path],
-        output_folder,
-        with_detail=False,
-        with_preview=True,
-        workspace_id=workspace_id,
-    )
+def get_preview_image(workspace_id, input_path, output_folder):
+    file_hash = calculate_file_hash(input_path)
 
     # get the proper filename, rename it because the generated PNG file has a _pt appendix
     file_name = os.path.basename(input_path).split(".")[0]
-    image_path = os.path.join(
-        output_folder, file_name + str("_pt") + str(".png"))
+
+    image_path = os.path.join(output_folder, f"{file_name}_{file_hash}_pt.png")
+
+    if not os.path.exists(image_path):
+        aps.generate_thumbnails(
+            [input_path],
+            output_folder,
+            with_detail=False,
+            with_preview=True,
+            workspace_id=workspace_id,
+        )
+        os.rename(os.path.join(output_folder, f"{file_name}_pt.png"), image_path)
 
     return image_path
 
@@ -158,24 +174,9 @@ def count_tokens(in_prompt, model="gpt-4o-mini"):
     return len(tokens)
 
 
-def get_assistant():
-    assistant_name = "Anchorpoint AI file tagger"
-    all_assistants = client.beta.assistants.list()
-    for assistant in all_assistants:
-        if assistant.name == assistant_name:
-            return assistant
-
-    my_assistant = client.beta.assistants.create(
-        instructions="When asked a question, write and run Python code to answer the question.",
-        name="Anchorpoint AI file tagger",
-        model="gpt-4o",
-        response_format=response_format)
-    return my_assistant
-
-
 def get_openai_response_images(in_prompt, image_paths: list[str], model="gpt-4o-mini") -> list[Any]:
-    if len(image_paths) == 0 or len(image_paths) > 16:
-        raise ValueError("The number of images should be between 1 and 16")
+    if len(image_paths) == 0 or len(image_paths) > images_per_request:
+        raise ValueError(f"The number of images should be between 1 and {images_per_request}")
 
     uploads_base64 = []
     for image_path in image_paths:
@@ -204,42 +205,6 @@ def get_openai_response_images(in_prompt, image_paths: list[str], model="gpt-4o-
         result = response.choices[0].message.content.strip()
         parsed = json.loads(result)
         return parsed['tags']
-    except openai.OpenAIError as e:
-        print(f"Error: {e}")
-        return []
-
-
-def get_openai_response_images_assistant(in_prompt, thread_id, image_paths: list[str], model="gpt-4o-mini") -> list[
-    str]:
-    if len(image_paths) == 0 or len(image_paths) > 16:
-        raise ValueError("The number of images should be between 1 and 16")
-
-    uploads = []
-    try:
-        for image_path in image_paths:
-            with open(image_path, "rb") as f:
-                uploads.append(client.files.create(file=f, purpose="assistants"))
-    except openai.OpenAIError as e:
-        print(f"Error: {e}")
-        return []
-
-    try:
-        assistant = get_assistant()
-
-        response = client.beta.threads.messages.create(
-            thread_id,
-            role="user",
-            content="Tag these images",
-            attachments=[{"file_id": upload.id, "tools": [{"type": "file_search"}]} for upload in uploads]
-        )
-
-        run = client.beta.threads.runs.create_and_poll(
-            thread_id=thread_id,
-            assistant_id=assistant.id,
-            instructions="Please address the user as Jane Doe. The user has a premium account."
-        )
-        print(run)
-        return response
     except openai.OpenAIError as e:
         print(f"Error: {e}")
         return []
@@ -289,24 +254,53 @@ def check_or_update_attribute(attribute: aps.Attribute, tag: str, database: aps.
     raise ValueError(f"Tag {tag} not found in the attribute tags: {anchorpoint_tag_names}")
 
 
+def change_slices_to_skip(workspace_id, database):
+    new_previews = []
+    prev_count = 0
+
+    global previews_sliced
+    for previews in previews_sliced:
+        for preview in previews:
+            original_file = original_files[preview]
+            prev_count += 1
+            ai_types_attr: Union[aps.apsync.Attribute, str] = database.attributes.get_attribute_value(original_file, "AI-Types")
+            if ai_types_attr and len(ai_types_attr) > 0:
+                continue
+
+            new_previews.append(preview)
+
+    new_previews_sliced = [
+        new_previews[i:i + images_per_request] for i in
+        range(0, len(new_previews), images_per_request)]
+    print(f"Reduced previews from {prev_count} to {len(new_previews)}")
+    previews_sliced = new_previews_sliced
+
+
 def proceed_callback(workspace_id, database):
     proceed_dialog.close()
+    skip_existing_tags = proceed_dialog.get_value("skip_existing_tags")
+    if skip_existing_tags:
+        change_slices_to_skip(workspace_id, database)
 
     def run():
         progress = ap.Progress("Requesting AI tags", "Processing", infinite=False, show_loading_screen=True)
         progress.report_progress(0)
-        # thread = client.beta.threads.create()
         for i, previews in enumerate(previews_sliced):
             response = get_openai_response_images(prompt, previews)
             progress.report_progress((i + 1) / len(previews_sliced))
             print(response)
             if len(response) < len(previews):
-                ap.UI().show_error("Error", f"Not all images were tagged {len(response)} < {len(previews)}")
-                raise ValueError(f"Not all images were tagged {len(response)} < {len(previews)}")
+                ap.UI().navigate_to_folder(initial_folder)
+                ap.UI().show_error("Error",
+                                   f"Not all images were tagged [Received {len(response)}, requested {len(previews)}]")
+                raise ValueError(f"Not all images were tagged [Received {len(response)}, requested {len(previews)}]")
+
             progress2 = ap.Progress("Updating tags", "Processing", infinite=False, show_loading_screen=True)
             for j, preview in enumerate(previews):
                 progress2.report_progress(j / len(previews))
                 tags = response[j]
+
+                ap.UI().navigate_to_folder(os.path.dirname(original_files[previews[j]]))
 
                 types = tags["types"]
                 types_tags = aps.AttributeTagList()
@@ -339,6 +333,7 @@ def proceed_callback(workspace_id, database):
             progress2.finish()
 
         progress.finish()
+        ap.UI().navigate_to_folder(initial_folder)
 
     ctx = ap.get_context()
     ctx.run_async(run)
@@ -346,18 +341,19 @@ def proceed_callback(workspace_id, database):
 
 def process_images(workspace_id, input_paths, database):
     if len(input_paths) == 0:
+        ap.UI().navigate_to_folder(initial_folder)
         ap.UI().show_error("No supported files selected", "Please select files to tag")
+        print("No supported files selected")
         return
 
     # start progress
-    progress = ap.Progress("Generating thumbnails", "Processing", infinite=False, show_loading_screen=True)
-    # Loop through each input path
+    progress = ap.Progress("Generating previews", "Processing", infinite=False, show_loading_screen=True)
     output_folder = create_temp_directory()
 
     previews = []
     print("Output folder: {}".format(output_folder.replace("\\", "\\\\")))
     for i, input_path in enumerate(input_paths):
-        image_path = get_thumbnail_image(workspace_id, input_path, output_folder)
+        image_path = get_preview_image(workspace_id, input_path, output_folder)
         previews.append(image_path)
         original_files[image_path] = input_path
         progress.report_progress(i / len(input_paths))
@@ -372,8 +368,8 @@ def process_images(workspace_id, input_paths, database):
         progress.report_progress(i / len(previews))
 
     global previews_sliced
-    # slice previews by 16
-    previews_sliced = [previews[i:i + 16] for i in range(0, len(previews), 16)]
+    # slice previews by images_per_request
+    previews_sliced = [previews[i:i + images_per_request] for i in range(0, len(previews), images_per_request)]
 
     # calculate token count
     pixel_price = pixel_count * input_pixel_price
@@ -395,6 +391,7 @@ def process_images(workspace_id, input_paths, database):
                             f"\nPixel count: {pixel_count}"
                             f"\nPrice: ~${total_price}"
                             f"\n\nProceed?")
+    proceed_dialog.add_checkbox(False, None, var="skip_existing_tags").add_text("Skip existing tags")
     (
         proceed_dialog
         .add_button("OK", callback=lambda d: proceed_callback(workspace_id, database))
@@ -444,6 +441,18 @@ ignored_extensions = [
 ]
 
 
+def get_all_files_recursive(folder_path) -> list[str]:
+    files = []
+    for root, _, file_names in os.walk(folder_path):
+        for file_name in file_names:
+            files.append(str(os.path.join(root, file_name)))
+
+    return files
+
+
+initial_folder = ""
+
+
 def main():
     ctx = ap.get_context()
     database = ap.get_api()
@@ -455,7 +464,22 @@ def main():
     global attributes
     attributes = [types_attribute, genres_attribute, objects_attribute]
 
-    filtered_files = filter_ignored_extensions(ctx.selected_files, ignored_extensions)
+    selected_files = ctx.selected_files
+
+    selected_folders = ctx.selected_folders
+
+    print(selected_folders)
+
+    if len(selected_folders) > 0:
+        for folder in selected_folders:
+            inner_files = get_all_files_recursive(folder)
+            print(inner_files)
+            selected_files.extend(inner_files)
+
+    filtered_files = filter_ignored_extensions(selected_files, ignored_extensions)
+
+    global initial_folder
+    initial_folder = ctx.path
 
     ctx.run_async(process_images, ctx.workspace_id, filtered_files, database)
 
