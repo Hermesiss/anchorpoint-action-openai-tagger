103131bae4b276f63d7ad498c00f1477b1652ae0
Hermesis <hermesiss@gmail.com>
2025-01-08 17:05:12 +0100

    Add links

--- package_settings.py
+++ package_settings.py
@@ -109,7 +109,7 @@ def main():
     dialog.add_checkbox(settings.file_label_ai_genres, var="file_label_ai_genres").add_text("Label Genres")
     (
         dialog.add_checkbox(settings.file_label_ai_objects, var="file_label_ai_objects")
-        .add_text("Label Objects. Count:")
+        .add_text(F"Label Objects{' ' * 25}Count:")
         .add_input(str(settings.file_label_ai_objects_min), var="file_label_ai_objects_min", width=50)
         .add_text("-")
         .add_input(str(settings.file_label_ai_objects_max), var="file_label_ai_objects_max", width=50)
@@ -117,12 +117,22 @@ def main():
     dialog.end_section()
 
     dialog.start_section("Folder Settings", folded=False)
-    dialog.add_checkbox(settings.folder_use_ai_engines, var="folder_use_ai_engines", text="Use Engines")
-    dialog.add_checkbox(settings.folder_use_ai_types, var="folder_use_ai_types", text="Use Types")
-    dialog.add_checkbox(settings.folder_use_ai_genres, var="folder_use_ai_genres", text="Use Genres")
+    dialog.add_checkbox(settings.folder_use_ai_engines, var="folder_use_ai_engines").add_text("Use Engines")
+    dialog.add_checkbox(settings.folder_use_ai_types, var="folder_use_ai_types").add_text("Use Types")
+    dialog.add_checkbox(settings.folder_use_ai_genres, var="folder_use_ai_genres").add_text("Use Genres")
     dialog.end_section()
 
     dialog.add_button("Apply", callback=apply_callback)
+
+    dialog.add_separator()
+    (
+        dialog
+        .add_info("<a href='https://platform.openai.com/settings/organization/api-keys'>API Keys</a>")
+        .add_info("<a href='https://platform.openai.com/settings/organization/usage'>Usage</a>")
+        .add_info("&nbsp;" * 70)
+        .add_info("<a href='https://ko-fi.com/hermesistrismegistus'>Donate ♥</a>")
+    )
+
     dialog.show()
 
 
